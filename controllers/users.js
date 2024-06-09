4af7cbdaa90a126f52a6395c5dbf00f5279342a5
Christopher <cagm96@gmail.com>
2024-06-09 15:48:20 -0400

    update

--- controllers/users.js
+++ controllers/users.js
@@ -8,31 +8,34 @@ const {
 const getUsers = (req, res) => {
   User.find({})
     .orFail(() => {
-      const error = new Error("Users ID not found");
-      error.statusCode = 400;
-      throw error;
+      // const error = new Error("Users ID not found");
+      // error.statusCode = 400;
+      // throw error;
     })
     .then((users) => {
       res.send(users);
     })
     .catch((err) => {
       console.error("getUsers error name", err.name);
-      return res.status(default_error_500).send({ message: err.message });
+      // return res.status(default_error_500).send({ message: err.message });
     });
 };
 const getUser = (req, res) => {
   console.log("user id: ", req.params.userID);
   User.findById(req.params.userID)
     .orFail(() => {
-      // const error = new Error("User ID not found");
-      // error.statusCode = 404;
-      // throw error;
+      const error = new Error("User ID not found");
+      error.statusCode = 400;
+      throw error;
     })
     .then((user) => {
       res.send({ data: user });
     })
     .catch((err) => {
       console.error("getUser error name", err.name);
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: err.message });
+      }
       // return res.status(default_error_500).send({ message: err.message });
     });
 };
