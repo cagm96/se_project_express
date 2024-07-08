25025d217af0fcbfdc4cb26ff8a985f7d2d01f0e
Christopher <cagm96@gmail.com>
2024-07-07 20:31:21 -0400

    update to routesadded auth

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const auth = require("../middlewares/auth");
 const {
   createItem,
   getItems,
@@ -9,14 +9,14 @@ const {
   dislikeItem,
 } = require("../controllers/clothingItem");
 
-router.post("/", createItem);
+router.post("/", auth, createItem);
 
 router.get("/", getItems);
 
-router.delete("/:itemId", deleteItem);
+router.delete("/:itemId", auth, deleteItem);
 
-router.put("/:itemId/likes", likeItem);
+router.put("/:itemId/likes", auth, likeItem);
 
-router.delete("/:itemId/likes", dislikeItem);
+router.delete("/:itemId/likes", auth, dislikeItem);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const clothingItem = require("./clothingItem");
 const userRouter = require("./users");
 const { invalidData404 } = require("../utils/errors");
 const { login, createUser } = require("../controllers/users");
+const { auth } = require("../middlewares/auth");
 
-router.use("/users", userRouter);
-router.use("/items", clothingItem);
+router.use("/users", auth, userRouter);
+router.use("/items", auth, clothingItem);
 router.post("/signin", login);
 router.post("/signup", createUser);
 
